5b4e481f9664702675df67d808acb9df8adb33e1
Golden <goldenisaprotogen@tutamail.com>
2025-11-14 22:22:51 -0500

    Add files via upload

---
+++ GoldensGorillaNametags/GoldensGorillaNametags/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace GoldensGorillaNametags
+{
+    internal class Constants
+    {
+        //MAJOR.MINOR.PATCH
+        public const string Guid = "golden.gorillanametags";
+        public const string Name = "GoldensGorillaNametags";
+        public const string Version = "4.2.0";
+    }
+}
